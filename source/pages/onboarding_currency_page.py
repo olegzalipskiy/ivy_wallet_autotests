fc5c397a0d07c5dab6f51486d69a06aa609d6a8d
oleh_zalipskyi <angeloff92@gmail.com>
2023-06-10 15:56:55 +0300

    Added check elements visibility

--- source/pages/onboarding_currency_page.py
+++ source/pages/onboarding_currency_page.py
@@ -1,7 +1,10 @@
+import json
+
 from appium.webdriver.common.mobileby import MobileBy
 
 from source.pages.base_page import BasePage
 from support.helpers.mobile_locator import MobileLocator
+from support.utils.file_system import FileSystem
 
 
 class OnboardingCurrencyPage(BasePage):
@@ -47,6 +50,47 @@ class OnboardingCurrencyPage(BasePage):
 
     _CANCEL_SEARCH_BUTTON = _SEARCH_BUTTON
 
+    @staticmethod
+    def generate_currency_locator(currency: str) -> MobileLocator:
+        return MobileLocator(
+            android=(MobileBy.XPATH,
+                     f'//android.widget.TextView[@text="{currency}"]')
+        )
+
+    def check_title_is_displayed(self):
+        return self.driver.check.is_element_displayed(by_method_with_selector=self._TITLE)
+
+    def check_suggested_title_is_displayed(self):
+        return self.driver.check.is_element_displayed(by_method_with_selector=self._SUGGESTED_TITLE)
+
+    def check_cross_button_is_displayed(self):
+        return self.driver.check.is_element_displayed(by_method_with_selector=self._CROSS_BUTTON)
+
+    def check_search_button_is_displayed(self):
+        return self.driver.check.is_element_displayed(by_method_with_selector=self._SEARCH_BUTTON)
+
+    def check_choose_button_is_displayed(self):
+        return self.driver.check.is_element_displayed(by_method_with_selector=self._CHOOSE_BUTTON)
+
+    def check_search_field_is_displayed(self):
+        return self.driver.check.is_element_displayed(by_method_with_selector=self._SEARCH_FIELD)
+
+    def check_search_field_placeholder_is_displayed(self):
+        return self.driver.check.is_element_displayed(by_method_with_selector=self._SEARCH_PLACEHOLDER)
+
+    def check_search_icon_is_displayed(self):
+        return self.driver.check.is_element_displayed(by_method_with_selector=self._SEARCH_ICON)
+
+    def check_cancel_search_button_is_displayed(self):
+        return self.driver.check.is_element_displayed(by_method_with_selector=self._CANCEL_SEARCH_BUTTON)
+
+    def check_all_suggested_currency_are_displayed(self):
+        currency_path = FileSystem.get_absolute_path("resources", "test_data", "suggested_currency_list.json")
+        with open(currency_path) as currency_file:
+            currency_list = json.load(currency_file)
+        for currency in currency_list:
+            return self.driver.check.is_element_displayed(by_method_with_selector=self.generate_currency_locator(currency))
+
 
 class SelectedCurrencyElement(BasePage):
 
@@ -56,3 +100,6 @@ class SelectedCurrencyElement(BasePage):
             android=(MobileBy.XPATH,
                      f'//android.widget.TextView[@text="Selected"]/preceding-sibling::android.widget.TextView[@text="{currency}"]')
         )
+
+    def check_selected_currency_is_displayed(self, currency: str):
+        return self.driver.check.is_element_displayed(self.generate_selected_currency_locator(currency))
